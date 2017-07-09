65f4f7e2dfb548946e670edf128f9e94961c5d25
mohsin <patelmohsin2624@gmail.com>
2017-07-09 11:53:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,363 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Mohsin Patel</title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery-1.11.0.min.js"></script>
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--- Jquery--->
+<!--- Goog Fonts -->
+<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
+<link rel="icon" type="image/ico" href="images/hat.ico" />
+</head>
+<body>
+	<!-- Header Starts Here ---->
+	<div class="header">
+		<div class="container">
+			<div class="header-profile">
+				<img src="images/profile.jpg" alt="mohsin patel" />
+			</div>
+		</div>
+	</div>
+	<br><br><br><br>
+	<!-- Header Ends Here ---->
+	<!--- About Me Starts Here --->
+	<div class="about">
+		<div class="container">
+			<div class="about-top">
+				<h3 class="top-title">Mohsin Patel</h3>
+				<span class="line"></span>
+				<p class="about-p">I am Mohsin Patel,
+            an IT engineer, very keen to do new things in life.</p>
+				<p>I think the perfect way to learn something is doing it practically.With my good leadership skills I can guide a group of people to do innovative things and help them to be better.<br><br>
+                <b>Entreprenure.Web Developer.Cricketer.Leader.Hobbyist.Social Worker</p>
+				<div class="row about-row">
+					<div class="col-md-4 about-row-column">
+						<ul class="about-details">
+							<li><p>Birthday: <span>24 Nov 1995</span></p></li>
+							<li><p>Phone: <span>+91 9974005112</span></p></li>
+							<li><p>Email:  <span><a href="mailto:patelmohsin2624@gmail.com">patelmohsin2624@gmail.com</a></span></p></li>
+							<!--<li><p>Website: <span><a href="http://w3layouts.com">www.Loremipsy.me</a></span></p></li>
+							<li><p>Address: <span>123 Ipsy Street, Lorem, Ipsy</span></p></li>-->
+						</ul>
+					</div>
+					<div class="col-md-8 about-row-column">
+						<h3>HOBBIES & INTERESTS</h3>
+						<ul class="hoby">
+							
+							<li><i class="car"></i></li>
+							
+							
+							<li><i class="reading"></i></li>
+							<li><i class="travel"></i></li>
+							
+						</ul>
+						<a href="" onclick="myfun()" class="download"><span></span>Download Resume</a>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--- About Me Ends Here --->
+	<!--- Experience Starts Here --->
+	<div class="experience">
+		<div class="container">
+			<div class="exp-top">
+				<h3 class="top-title">About</h3>
+				<span class="line"></span>
+			</div>
+			<div class="exp-devide">
+				<h4>Experience</h4>
+				<span class="devide-line">
+				</span>
+				<label class="bag"></label>
+				<div class="exp-devide-grid-right">
+					<h5>PHP Developer</h5>
+					<small>Rishabh Software Pvt. Ltd - 2017 . Current</small>
+					<p></p>
+				</div>
+				<div class="exp-devide-grid-left">
+					<h5>Internship</h5>
+					<small>RISE - Dec 2016 . June 2017</small>
+					<p></p>
+				</div>
+				<!--<div class="exp-devide-grid-rightb">
+					<h5>UI/UX DESIGNER</h5>
+					<small>IBM - 2006 . 2010</small>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+				</div>-->
+			</div>
+			<div class="exp-devide education">
+				<h4>Education</h4>
+				<span class="devide-line">
+				</span>
+				<label class="book"></label>
+				<div class="exp-devide-grid-right">
+					<h5>GRADUATION</h5>
+					<small>Gujarat Technological University - Bachelor of Engineer (IT)   </small>
+					<p>Shri S'ad Vidya Mandal Institute of Technology,Bharuch 2013 . 2017</p>
+				</div>
+				<div class="exp-devide-grid-left">
+					<h5>High School</h5>
+					<small>GSHEB,Gandhinagar </small>
+					<p>Haji A M Motala Sarvajanik Higher Secondary School-Kosamba 2011 . 2013</p>
+				</div>
+				<div class="exp-devide-grid-rightb">
+					<h5>SSC/10th</h5>
+					<small>GSEB,Gandhinagar </small>
+					<p>Haji A M Motala Sarvajanik School-Kosamba 2011 . 2013</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--- Experience Ends Here --->
+	<!--- Skills Starts Here --->
+	<!--<div class="skills">
+		<div class="container">
+			<div class="skills-top">
+				<h3 class="top-title">Skills</h3>
+				<span class="line"></span>
+				<div class="row skill-grids text-center">
+									<div class="col-md-3 skill-column">	
+										<div class="skill-grid">
+											<div class="circle" id="circles-1"></div>									
+											 <h3>PHOTOSHOP</h3>	
+											 
+										 </div>								  
+									</div>
+									<div class="col-md-3 skill-column">	
+										<div class="skill-grid">
+											<div class="circle" id="circles-2"></div>									
+											<h3>ILLUSTRATOR</h3>	
+											 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+										 </div>								  
+									</div>
+									<div class="col-md-3 skill-column">	
+										<div class="skill-grid">
+											<div class="circle" id="circles-3"></div>									
+											 <h3>HTML/CSS</h3>	
+											 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+										 </div>								  
+									</div>
+									<div class="col-md-3 skill-column">	
+										<div class="skill-grid">
+											<div class="circle" id="circles-4"></div>									
+											 <h3>JAVASCRIPT</h3>
+											 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>	
+										 </div>								  
+									</div>
+									<div class="clearfix"> </div>
+					          </div>
+		</div>
+	</div>
+			<!--- Skill Script --->
+				<!--<script type="text/javascript" src="js/circles.js"></script>-->
+						        <!-- <script>
+									var colors = [
+											['#fff', '#2980b9'], ['#fff', '#2980b9'], ['#fff', '#2980b9'], ['#fff', '#2980b9']
+										];
+									for (var i = 1; i <= 6; i++) {
+										var child = document.getElementById('circles-' + i),
+											percentage = 50 + (i * 10);
+											
+										Circles.create({
+											id:         child.id,
+											percentage: percentage,
+											radius:     80,
+											width:      15,
+											number:   	percentage / 10,
+											text:       '%',
+											colors:     colors[i - 1]
+										});
+									}
+							
+					</script>-->
+			<!--- Skill Script --->
+	<!--</div>-->
+	<!--- Skills Ends Here --->
+	<!-- Social Media Connections ---->
+	<div class="social-media">
+		<div class="container">
+			<div class="social-media-top">
+				<h3 class="top-title">SOCIAL MEDIA CONNECTIONS</h3>
+				<span class="line"></span>
+				<ul class="social">
+					<li><a href="https://www.linkedin.com/in/mohsin-patel-1007"  target="_blank"><i class="li"></i></a></li>
+					<li><a href="https://www.twitter.com/mohsin_svmit" target="_blank"><i class="tw"></i></a></li>
+					<li><a href="https://www.instagram.com/mohsin_it"  target="_blank"><i class="ph"></i></a></li>
+					<li><a href="#"><i class="fb"></i></a></li>							
+					<!--<li><a href=""><i class="gp"></i></a></li>-->
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- Social Media Connections ---->
+	<!--- Awards Starts Here ---->
+	<!--<div class="awards">
+		<div class="container">
+			<div class="awards-top">
+				<!--<h3 class="top-title">AWARDS & ACHIEVEMENTS</h3>
+				<span class="line"></span>
+				<div class="row award-row">
+					<div class="col-md-3 award-column">
+						<i class="award"></i>
+						<h5>50</h5>
+						<small>Awards Won!</small>
+					</div>
+					<div class="col-md-3 award-column">
+						<i class="project"></i>
+						<h5>570</h5>
+						<small>PROJECTS DONE</small>
+					</div>
+					<div class="col-md-3 award-column">
+						<i class="client"></i>
+						<h5>215</h5>
+						<small>HAPPY CLIENTS</small>
+					</div>
+					<div class="col-md-3 award-column">
+						<i class="cups"></i>
+						<h5>2145</h5>
+						<small>CUPS OF COFFEE</small>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--- Awards Ends Here ---->
+	<!--- Twitter Feed Starts Here ---->
+	<!--<div class="twitter">
+		<div class="container">
+			<div class="twitter-top">
+				<i class="twit"></i>
+				<span class="line"></span>
+				<!-- Slideshow 4 
+			    <div  id="top" class="callbacks_container">
+			      <ul class="rslides" id="slider4">
+			        <li>
+			          <div class="slider-top">
+			          	 	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem<br>Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+			          </div>
+			        </li>
+			        <li>
+			          <div class="slider-top">
+			          	<p>simply dummy text of the Lorem Ipsum is printing and typesetting industry. Lorem<br>Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+			          </div>
+			        </li>
+			        <li>
+			          <div class="slider-top">
+			          	<p>printing and typesetting Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem<br>Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+			          </div>
+			        </li>
+			        <li>
+			          <div class="slider-top">
+			          	<p>and typesetting Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem<br>Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+			          </div>
+			        </li>
+			        <li>
+			          <div class="slider-top">
+			          	<p>typesetting Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem<br>Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+			          </div>
+			        </li>
+			      </ul>
+			    </div>
+			    <div class="clearfix"> </div>
+			</div>-->
+			<!--- Slider Script --->
+					<script src="js/responsiveslides.min.js"></script>
+					 <script>
+					    // You can also use "$(window).load(function() {"
+					    $(function () {
+					      // Slideshow 4
+					      $("#slider4").responsiveSlides({
+					        auto: true,
+					        pager: true,
+					        nav: true,
+					        speed: 500,
+					        namespace: "callbacks",
+					        before: function () {
+					          $('.events').append("<li>before event fired.</li>");
+					        },
+					        after: function () {
+					          $('.events').append("<li>after event fired.</li>");
+					        }
+					      });
+					
+					    });
+					  </script>
+			<!----//End-slider-script---->
+
+		<!--</div>
+	</div>-->
+	<!--- Twitter Feed Ends Here ---->
+	<!--- Contact Starts Here ---->
+	<div class="contact">
+		<div class="container">
+			<div class="contact-top">
+				<h3 class="top-title">Contact Me</h3>
+				<span class="line"></span>
+				
+				<div class="row contact-row">
+					<!--<div class="col-md-4 contact-column">
+						<i class="loc"></i>
+						<p>123 Ipsy Street,<br> Lorem, Ipsy</p>
+					</div>-->
+					<div class="col-md-4 contact-column col-md-offset-4">
+						
+						<center><p><a href="mailto:patelmohsin2624@gmail.com"><i class="mail"></i></a></p>
+					</div>
+					<!--<div class="col-md-4 contact-column">
+						<i class="phone"></i>
+						<p>+123 456 789<br>+987 654 321</p>
+					</div>-->
+					<div class="clearfix"></div>
+					<strong>Or</strong>
+				</div>
+				
+				<div class="contact-form">
+					<div class="text">
+						<input type="text" value="Your Name.." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Your Name..';}"/>
+					</div>
+					<div class="text">
+						<input type="text" value="Your Email.." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Your Email..';}"/>
+					</div>
+					<div class="text">
+						<textarea value="Message:" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Message';}">Your Message..</textarea>
+					</div>
+					<div class="text">
+						<input type="submit" onclick="myfun()"/>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--- Contact Ends Here ---->
+	<!--- Footer Starts Here ---->
+	<div class="footer">
+		<div class="container">
+			<p class="copyright">Copyright &COPY;<?php echo date('Y')?>   Mohsin Patel </p>
+		</div>
+	</div>
+	<!--- Footer Ends Here ---->
+<script>
+function myfun(){
+alert("Sorry! Server is down. Drop email on patelmohsin2624@gmail.com");
+}
+</script>
+</body>
+</html>
+
+
+
